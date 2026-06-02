ac76c28f69a98e570dac75306cae7cd0c981f726
connor-mellor <Connor_Mellor@hotmail.com>
2026-06-02 23:09:54 +0100

    chore: create plain react+vite repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <h1> Bills little Uniform </h1>
+      </section>
+    </>
+  )
+}
+
+export default App
